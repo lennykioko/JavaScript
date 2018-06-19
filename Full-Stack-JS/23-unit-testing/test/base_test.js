9a3342f39b9ac61c47d468dfc36901fef09460b8
unknown <lennykmutua@gmail.com>
2018-06-19 09:23:16 +0300

    Go through videos

---
+++ Full-Stack-JS/23-unit-testing/test/base_test.js
@@ -0,0 +1,9 @@
+const expect = require("chai").expect;
+
+// test suite
+describe("Mocha", () => {
+    // test spec (unit test)
+    it("Should run tests using npm", () => {
+        expect(true).to.be.ok;
+    });
+});
